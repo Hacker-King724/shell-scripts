c31b8cbf6bd9d0e79927f3ce3db59826974bb2e0
Hacker-King724 <kali.something1972@proton.me>
2026-06-18 13:39:05 +0530

    variables demo

---
+++ 03_vardemo.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# in this script i will use some variables
+
+name="Dipanjan"
+age=21
+
+echo "Hello there! I'm $name and my age is $age"
+
+HOSTNAME=$(hostname)
+echo "And here is my hostname: $HOSTNAME"
+
